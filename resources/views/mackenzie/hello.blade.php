9e6776a657ac552018dbee8c8e115132ed13f225
davisgibson <vagrant@homestead>
2020-04-08 16:42:11 +0000

    added bootstrap

--- resources/views/mackenzie/hello.blade.php
+++ resources/views/mackenzie/hello.blade.php
@@ -1,3 +1,8 @@
-@yield('stylesheets')
-
-<p style="margin:auto; width:50%; text-align:center; height:50%">hello<a style="text-decoration: none;" href="/mackenzie/loveyou">.</a></p>
+@extends('layout')
+@section('content')
+	<div class="d-flex justify-content-center">
+		<div class="d-flex justify-content-center">
+			<p class="align-middle text-center">hello<a style="text-decoration: none;" href="/mackenzie/loveyou">.</a></p>
+		</div>
+	</div>
+@endsection
